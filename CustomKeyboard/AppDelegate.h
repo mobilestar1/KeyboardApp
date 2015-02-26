fee79cffd6911b0c1b652c7c36cd78a0db004ff9
JangJiang <sky@Skys-iMac.local>
2015-02-26 16:58:13 +0800

    Initial Commit

---
+++ CustomKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomKeyboard
+//
+//  Created by Sky on 2/26/15.
+//  Copyright (c) 2015 Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomKeyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomKeyboard
+//
+//  Created by Sky on 2/26/15.
+//  Copyright (c) 2015 Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
